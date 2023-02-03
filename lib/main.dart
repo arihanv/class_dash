3afec305c3242cee783c094110a22f8c7e429bff
AkhilKrishna16 <akhil.krishnamurthy2006@gmail.com>
2023-02-03 01:38:21 -0600

    added runApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@
 
 import 'package:flutter/material.dart';
 
-
 import './header.dart';
 import 'current_events.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
